7a8b0a7074433dcbaec81165a5505e6247852b1f
goto_emi <yuzukkoemo@gmail.com>
2024-04-14 23:18:37 +0900

    team2のアプリです

--- super_app.py
+++ super_app.py
@@ -1,13 +1,19 @@
 import streamlit as st
+from faker import Faker
+
 def app():
     st.title("HAGAKUREの凄いアプリです")
     st.image("hagakurekun.png",width=200)
-    tab1,tab2 = st.tabs(["page1","page2"])
+    tab1,tab2 = st.tabs(["page1","職業探し"])
     with tab1:
         st.write("page1です")
         st.text("ここをチーム１が編集します!")
     with tab2:
-        st.write("page2です")
-        st.text("ここをチーム２が編集します!")
+        st.title("あなたの適職は？")
+        fake = Faker("jp-JP")
+        name = st.text_input("名前を入力してください")
+        if name is not "":
+            fakejob = fake.job()
+            st.text(f"{name}さんの適職は、{fakejob}です。")
 if __name__ == '__main__':
     app()
